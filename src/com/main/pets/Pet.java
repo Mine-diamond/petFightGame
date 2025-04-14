793f0c56e270fb2582548978dfb9c1e61ad0ef90
mine_ <mine_yaoym@outlook.com>
2025-04-14 21:24:42 +0800

    将部分方法暴露到了Pet类下

--- src/com/main/pets/Pet.java
+++ src/com/main/pets/Pet.java
@@ -185,6 +185,309 @@ public abstract class Pet {
         }
     }
 
+
+    //------------------------HP相关方法--------------------------
+    /**
+     * 在基础HP最大值上增加指定值
+     * @param value 增加的值
+     * @param reason 原因说明
+     */
+    public void addBaseMaxHP(double value, String reason) {
+        stat.getHP().addBaseMaxValue(value, reason);
+    }
+    /**
+     * 设置基础HP最大值
+     * @param value 设置的值
+     * @param reason 原因说明
+     */
+    public void setBaseMaxHP(double value, String reason) {
+        stat.getHP().setBaseMaxValue(value, reason);
+    }
+    /**
+     * 为当前HP最大值添加一个加法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addHPAddModifier(double value, String id, String tag, int priority) {
+        stat.getHP().addCurrentMaxValueAddModifier(value, id, tag, priority);
+    }
+    /**
+     * 为当前HP最大值添加一个乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addHPMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getHP().addCurrentMaxValueMultiplyModifier(value, id, tag, priority);
+    }
+
+    /**
+     * 为当前HP最大值添加一个乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addHPBaseMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getHP().addCurrentMaxValueBaseMultiplyModifier(value, id, tag, priority);
+    }
+    /**
+     * 恢复HP到最大值
+     */
+    public void refillHP() {
+        stat.getHP().refill();
+    }
+    /**
+     * 增加HP值
+     * @param addValue 增加的值
+     * @return 是否未达到上限
+     */
+    public boolean addHP(double addValue) {
+        return stat.getHP().addValue(addValue);
+    }
+    /**
+     * 减少HP值
+     * @param removeValue 减少的值
+     * @return 是否未降至0
+     */
+    public boolean removeHP(double removeValue) {
+        return stat.getHP().removeValue(removeValue);
+    }
+    /**
+     * 获取当前HP值
+     * @return 当前HP
+     */
+    public double getCurrentHP() {
+        return stat.getHP().getValue().doubleValue();
+    }
+    /**
+     * 获取最大HP值
+     * @return 最大HP
+     */
+    public double getMaxHP() {
+        return stat.getHP().getCurrentMaxValue();
+    }
+    //------------------------能量相关方法--------------------------
+    /**
+     * 在基础能量最大值上增加指定值
+     * @param value 增加的值
+     * @param reason 原因说明
+     */
+    public void addBaseMaxEnergy(double value, String reason) {
+        stat.getEnergy().addBaseMaxValue(value, reason);
+    }
+    /**
+     * 设置基础能量最大值
+     * @param value 设置的值
+     * @param reason 原因说明
+     */
+    public void setBaseMaxEnergy(double value, String reason) {
+        stat.getEnergy().setBaseMaxValue(value, reason);
+    }
+    /**
+     * 为当前能量最大值添加一个加法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addEnergyAddModifier(double value, String id, String tag, int priority) {
+        stat.getEnergy().addCurrentMaxValueAddModifier(value, id, tag, priority);
+    }
+    /**
+     * 为当前能量最大值添加一个乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addEnergyMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getEnergy().addCurrentMaxValueMultiplyModifier(value, id, tag, priority);
+    }
+
+    /**
+     * 为当前能量最大值添加一个乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addEnergyBaseMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getEnergy().addCurrentMaxValueBaseMultiplyModifier(value, id, tag, priority);
+    }
+
+    /**
+     * 恢复能量到最大值
+     */
+    public void refillEnergy() {
+        stat.getEnergy().refill();
+    }
+    /**
+     * 增加能量值
+     * @param addValue 增加的值
+     * @return 是否未达到上限
+     */
+    public boolean addEnergy(double addValue) {
+        return stat.getEnergy().addValue(addValue);
+    }
+    /**
+     * 减少能量值
+     * @param removeValue 减少的值
+     * @return 是否未降至0
+     */
+    public boolean removeEnergy(double removeValue) {
+        return stat.getEnergy().removeValue(removeValue);
+    }
+    /**
+     * 检查能量是否足够
+     * @param value 需要检查的值
+     * @return 是否足够
+     */
+    public boolean hasEnoughEnergy(double value) {
+        return stat.getEnergy().canRemoveValue(value);
+    }
+    /**
+     * 获取当前能量值
+     * @return 当前能量
+     */
+    public double getCurrentEnergy() {
+        return stat.getEnergy().getValue().doubleValue();
+    }
+    /**
+     * 获取最大能量值
+     * @return 最大能量
+     */
+    public double getMaxEnergy() {
+        return stat.getEnergy().getCurrentMaxValue();
+    }
+    //------------------------攻击相关方法--------------------------
+    /**
+     * 设置基础攻击力
+     * @param value 设置的值
+     * @param reason 原因说明
+     */
+    public void setBaseAttack(double value, String reason) {
+        stat.getBaseAttack().setBaseValue(value, reason);
+    }
+    /**
+     * 增加基础攻击力
+     * @param value 增加的值
+     * @param reason 原因说明
+     */
+    public void addBaseAttack(double value, String reason) {
+        stat.getBaseAttack().modifyBaseValue(value, reason);
+    }
+    /**
+     * 为攻击力添加加法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addAttackAddModifier(double value, String id, String tag, int priority) {
+        stat.getBaseAttack().addAdditiveModifier(id, value, tag, priority);
+    }
+    /**
+     * 为攻击力添加乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addAttackMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getBaseAttack().addMultiplicativeModifier(id, value, tag, priority);
+    }
+    /**
+     * 为攻击力添加乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addAttackBaseMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getBaseAttack().addBaseMultiplicativeModifier(id, value, tag, priority);
+    }
+    /**
+     * 获取当前攻击力
+     * @return 当前攻击力
+     */
+    public double getCurrentAttack() {
+        return stat.getCurrentAttack().getValue();
+    }
+    /**
+     * 获取基础攻击力
+     * @return 基础攻击力
+     */
+    public double getBaseAttack() {
+        return stat.getBaseAttack().getCurrentBaseValue();
+    }
+    //------------------------防御相关方法--------------------------
+    /**
+     * 设置基础防御力
+     * @param value 设置的值
+     * @param reason 原因说明
+     */
+    public void setBaseDefense(double value, String reason) {
+        stat.getBaseDefense().setBaseValue(value, reason);
+    }
+    /**
+     * 增加基础防御力
+     * @param value 增加的值
+     * @param reason 原因说明
+     */
+    public void addBaseDefense(double value, String reason) {
+        stat.getBaseDefense().modifyBaseValue(value, reason);
+    }
+    /**
+     * 为防御力添加加法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addDefenseAddModifier(double value, String id, String tag, int priority) {
+        stat.getBaseDefense().addAdditiveModifier(id, value, tag, priority);
+    }
+    /**
+     * 为防御力添加乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addDefenseMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getBaseDefense().addMultiplicativeModifier(id, value, tag, priority);
+    }
+
+    /**
+     * 为防御力添加基础乘法修正
+     * @param value 修正值
+     * @param id 修正器ID
+     * @param tag 标签
+     * @param priority 优先级
+     */
+    public void addDefenseBAseMultiplyModifier(double value, String id, String tag, int priority) {
+        stat.getBaseDefense().addBaseMultiplicativeModifier(id, value, tag, priority);
+    }
+    /**
+     * 获取当前防御力
+     * @return 当前防御力
+     */
+    public double getCurrentDefense() {
+        return stat.getCurrentDefense().getValue();
+    }
+    /**
+     * 获取基础防御力
+     * @return 基础防御力
+     */
+    public double getBaseDefense() {
+        return stat.getBaseDefense().getCurrentBaseValue();
+    }
+
+
     //------------------------辅助方法--------------------------
     @Override
     public String toString() {
